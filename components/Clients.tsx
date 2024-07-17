7f3387342257eba004c9ba040889c17a74bdbfaf
ChaitanyaRawat <chaitanyarawat95@gmail.com>
2024-07-17 11:44:57 +0530

    fixing import bug

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
-import { companies, testimonials } from '@/data'
-import { div } from 'three/examples/jsm/nodes/Nodes.js'
+// import React from 'react'
+// import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+// import { companies, testimonials } from '@/data'
+// import { div } from 'three/examples/jsm/nodes/Nodes.js'
 
-const Clients = () => {
-  return (
-    <div className='py-20' id="testimonials">
-      <h1 className='heading'>
-        Kind words from
-        <span className='text-purple'> satisfied clients</span>
-      </h1>
-      <div className='flex flex-col items-center max-lg:mt-10'>
-
-
-        <InfiniteMovingCards
-          items={testimonials}
-          direction='right'
-          speed='slow'
-        />
-
-        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
-          {
-            companies.map(({ id, name, img, nameImg }) => (
-              <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
-
-                <img
-                  src={img}
-                  alt={name}
-                  className='md:w-10 w-5'
-                />
-                <img
-                  src={nameImg}
-                  alt={name}
-                  className='md:w-24 w-20'
-                />
-              </div>
-            ))
-          }
-        </div>
+// const Clients = () => {
+//   return (
+//     <div className='py-20' id="testimonials">
+//       <h1 className='heading'>
+//         Kind words from
+//         <span className='text-purple'> satisfied clients</span>
+//       </h1>
+//       <div className='flex flex-col items-center max-lg:mt-10'>
 
 
-      </div>
-    </div>
-  )
-}
+//         <InfiniteMovingCards
+//           items={testimonials}
+//           direction='right'
+//           speed='slow'
+//         />
 
-export default Clients
+//         <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+//           {
+//             companies.map(({ id, name, img, nameImg }) => (
+//               <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+
+//                 <img
+//                   src={img}
+//                   alt={name}
+//                   className='md:w-10 w-5'
+//                 />
+//                 <img
+//                   src={nameImg}
+//                   alt={name}
+//                   className='md:w-24 w-20'
+//                 />
+//               </div>
+//             ))
+//           }
+//         </div>
+
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Clients
